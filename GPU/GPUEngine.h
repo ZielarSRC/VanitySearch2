597c992b9efdd7b646d6e181c27b113153b3f4e5
Aleksander <poczta@zielar.pl>
2025-04-10 11:51:52 +0200

    Update GPUEngine.h

--- GPU/GPUEngine.h
+++ GPU/GPUEngine.h
@@ -1,107 +1,31 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef GPUENGINEH
-#define GPUENGINEH
-
-#include <vector>
-#include "../SECP256k1.h"
-
-#define SEARCH_COMPRESSED 0
-#define SEARCH_UNCOMPRESSED 1
-#define SEARCH_BOTH 2
-
-static const char *searchModes[] = {"Compressed","Uncompressed","Compressed or Uncompressed"};
-
-// Number of key per thread (must be a multiple of GRP_SIZE) per kernel call
-#define STEP_SIZE 1024
-
-// Number of thread per block
-#define ITEM_SIZE 28
-#define ITEM_SIZE32 (ITEM_SIZE/4)
-#define _64K 65536
-
-typedef uint16_t prefix_t;
-typedef uint32_t prefixl_t;
-
-typedef struct {
-  uint32_t thId;
-  int16_t  incr;
-  int16_t  endo;
-  uint8_t  *hash;
-  bool mode;
-} ITEM;
-
-// Second level lookup
-typedef struct {
-  prefix_t sPrefix;
-  std::vector<prefixl_t> lPrefixes;
-} LPREFIX;
+#pragma once
+#include <cuda_runtime.h>
+#include "GPUGroup.h"
+#include "GPUHash.h"
+#include "GPUMath.h"
 
 class GPUEngine {
-
 public:
+    // Konstruktor z obsługą wielu GPU
+    explicit GPUEngine(int deviceID = 0);
 
-  GPUEngine(int nbThreadGroup,int nbThreadPerGroup,int gpuId,uint32_t maxFound,bool rekey); 
-  ~GPUEngine();
-  void SetPrefix(std::vector<prefix_t> prefixes);
-  void SetPrefix(std::vector<LPREFIX> prefixes,uint32_t totalPrefix);
-  bool SetKeys(Point *p);
-  void SetSearchMode(int searchMode);
-  void SetSearchType(int searchType);
-  void SetPattern(const char *pattern);
-  bool Launch(std::vector<ITEM> &prefixFound,bool spinWait=false);
-  int GetNbThread();
-  int GetGroupSize();
+    // Nowoczesne zarządzanie pamięcią (CUDA 12+)
+    template<typename T>
+    T* allocateDeviceMemory(size_t count, cudaStream_t stream = 0);
 
-  bool Check(Secp256K1 *secp);
-  std::string deviceName;
+    template<typename T>
+    void freeDeviceMemory(T* ptr, cudaStream_t stream = 0);
 
-  static void PrintCudaInfo();
-  static void GenerateCode(Secp256K1 *secp, int size);
+    // Generowanie kluczy (zoptymalizowane pod Ampere)
+    __global__ void generateKeys(uint64_t* privateKeys, uint64_t* publicKeys, size_t count);
+
+    // Wyszukiwanie adresów (z wildcard)
+    __host__ __device__ bool matchAddress(const char* address, const char* pattern);
+
+    // Synchronizacja strumienia
+    void synchronizeStream(cudaStream_t stream = 0);
 
 private:
-
-  bool callKernel();
-  static void ComputeIndex(std::vector<int> &s, int depth, int n);
-  static void Browse(FILE *f,int depth, int max, int s);
-  bool CheckHash(uint8_t *h, std::vector<ITEM>& found, int tid, int incr, int endo, int *ok);
-
-  int nbThread;
-  int nbThreadPerGroup;
-  prefix_t *inputPrefix;
-  prefix_t *inputPrefixPinned;
-  uint32_t *inputPrefixLookUp;
-  uint32_t *inputPrefixLookUpPinned;
-  uint64_t *inputKey;
-  uint64_t *inputKeyPinned;
-  uint32_t *outputPrefix;
-  uint32_t *outputPrefixPinned;
-  bool initialised;
-  uint32_t searchMode;
-  uint32_t searchType;
-  bool littleEndian;
-  bool lostWarning;
-  bool rekey;
-  uint32_t maxFound;
-  uint32_t outputSize;
-  std::string pattern;
-  bool hasPattern;
-
+    int deviceID_;
+    cudaDeviceProp deviceProps_;
 };
-
-#endif // GPUENGINEH
