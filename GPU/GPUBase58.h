e5970056a1f742d2f86d7727c0499e2b0cc47198
Aleksander <poczta@zielar.pl>
2025-04-10 23:26:22 +0200

    Update GPUBase58.h

--- GPU/GPUBase58.h
+++ GPU/GPUBase58.h
@@ -16,12 +16,12 @@
 */
 
 // ---------------------------------------------------------------------------------
-// Base58
+// Base58 - Optimized for CUDA
 // ---------------------------------------------------------------------------------
 
-__device__ __constant__ char *pszBase58 = (char *)"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+__device__ __constant__ char pszBase58[59] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
-__device__ __constant__ int8_t b58digits_map[] = {
+__device__ __constant__ int8_t b58digits_map[256] = {
   -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
   -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
   -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
@@ -32,84 +32,66 @@ __device__ __constant__ int8_t b58digits_map[] = {
   47,48,49,50,51,52,53,54, 55,56,57,-1,-1,-1,-1,-1,
 };
 
-__device__ __noinline__ void _GetAddress(int type,uint32_t *hash,char *b58Add) {
-
-  uint32_t addBytes[16];
-  uint32_t s[16];
+__device__ __forceinline__ void _GetAddress(int type, uint32_t *hash, char *b58Add) {
+  uint32_t addBytes[16] = {0};
+  uint32_t s[8];
   unsigned char A[25];
-  unsigned char *addPtr = A;
+  unsigned char digits[128] = {0};
   int retPos = 0;
-  unsigned char digits[128];
 
-  switch (type) {
-
-  case P2PKH:
-    A[0] = 0x00;
-    break;
-
-  case P2SH:
-    A[0] = 0x05;
-    break;
-
-  }
-  memcpy(A + 1, (char *)hash, 20);
-
-  // Compute checksum
+  // Set address type prefix
+  A[0] = (type == P2PKH) ? 0x00 : 0x05;
   
-  addBytes[0] = __byte_perm(hash[0], (uint32_t)A[0], 0x4012);
+  // Copy hash (20 bytes) to A[1..20]
+  #pragma unroll 5
+  for (int i = 0; i < 5; i++) {
+    ((uint32_t *)(A + 1))[i] = hash[i];
+  }
+
+  // Prepare SHA256 input
+  addBytes[0] = __byte_perm(hash[0], A[0], 0x4012);
   addBytes[1] = __byte_perm(hash[0], hash[1], 0x3456);
   addBytes[2] = __byte_perm(hash[1], hash[2], 0x3456);
   addBytes[3] = __byte_perm(hash[2], hash[3], 0x3456);
   addBytes[4] = __byte_perm(hash[3], hash[4], 0x3456);
   addBytes[5] = __byte_perm(hash[4], 0x80, 0x3456);
-  addBytes[6] = 0;
-  addBytes[7] = 0;
-  addBytes[8] = 0;
-  addBytes[9] = 0;
-  addBytes[10] = 0;
-  addBytes[11] = 0;
-  addBytes[12] = 0;
-  addBytes[13] = 0;
-  addBytes[14] = 0;
   addBytes[15] = 0xA8;
 
+  // Compute first SHA256 round
   SHA256Initialize(s);
   SHA256Transform(s, addBytes);
 
-#pragma unroll 8
-  for (int i = 0; i < 8; i++)
+  // Prepare second SHA256 round
+  #pragma unroll 8
+  for (int i = 0; i < 8; i++) {
     addBytes[i] = s[i];
-
+  }
   addBytes[8] = 0x80000000;
-  addBytes[9] = 0;
-  addBytes[10] = 0;
-  addBytes[11] = 0;
-  addBytes[12] = 0;
-  addBytes[13] = 0;
-  addBytes[14] = 0;
   addBytes[15] = 0x100;
 
+  // Compute second SHA256 round
   SHA256Initialize(s);
   SHA256Transform(s, addBytes);
 
+  // Store checksum (last 4 bytes of hash)
   A[21] = ((uint8_t *)s)[3];
   A[22] = ((uint8_t *)s)[2];
   A[23] = ((uint8_t *)s)[1];
   A[24] = ((uint8_t *)s)[0];
 
-  // Base58
-
-  // Skip leading zeroes
-  while(addPtr[0] == 0) {
+  // Skip leading zeroes and add '1's to output
+  unsigned char *addPtr = A;
+  while (addPtr[0] == 0) {
     b58Add[retPos++] = '1';
     addPtr++;
   }
-  int length = 25-retPos;
+  int length = 25 - (addPtr - A);
 
+  // Base58 conversion
   int digitslen = 1;
-  digits[0] = 0;
   for (int i = 0; i < length; i++) {
     uint32_t carry = addPtr[i];
+    #pragma unroll 4
     for (int j = 0; j < digitslen; j++) {
       carry += (uint32_t)(digits[j]) << 8;
       digits[j] = (unsigned char)(carry % 58);
@@ -121,10 +103,10 @@ __device__ __noinline__ void _GetAddress(int type,uint32_t *hash,char *b58Add) {
     }
   }
 
-  // reverse
-  for (int i = 0; i < digitslen; i++)
-    b58Add[retPos++] = (pszBase58[digits[digitslen - 1 - i]]);
-
-  b58Add[retPos] = 0;
-
+  // Reverse and store Base58 result
+  #pragma unroll 4
+  for (int i = 0; i < digitslen; i++) {
+    b58Add[retPos++] = pszBase58[digits[digitslen - 1 - i]];
+  }
+  b58Add[retPos] = '\0';
 }
