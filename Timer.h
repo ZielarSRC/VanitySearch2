a0b0cabb7df66b279b61d2a0253d8684970e10f3
Aleksander <poczta@zielar.pl>
2025-04-11 06:07:00 +0200

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -1,48 +1,24 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef TIMERH
-#define TIMERH
-
-#include <time.h>
+#pragma once
+#include <chrono>
 #include <string>
-#ifdef WIN64
-#include <windows.h>
-#endif
 
 class Timer {
-
 public:
-  static void Init();
-  static double get_tick();
-  static void printResult(char *unit, int nbTry, double t0, double t1);
-  static std::string getResult(char *unit, int nbTry, double t0, double t1);
-  static int getCoreNumber();
-  static std::string getSeed(int size);
-  static void SleepMillis(uint32_t millis);
-
-#ifdef WIN64
-  static LARGE_INTEGER perfTickStart;
-  static double perfTicksPerSec;
-  static LARGE_INTEGER qwTicksPerSec;
-#else
-  static time_t tickStart;
-#endif
-
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = std::chrono::time_point<Clock>;
+    
+    Timer() : startTime(Clock::now()) {}
+    
+    void reset() { 
+        startTime = Clock::now(); 
+    }
+    
+    double elapsed() const {
+        return std::chrono::duration<double>(Clock::now() - startTime).count();
+    }
+    
+    static std::string formatTime(double seconds);
+    
+private:
+    TimePoint startTime;
 };
-
-#endif // TIMERH
