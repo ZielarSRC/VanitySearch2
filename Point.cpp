8b89c6ccb2db6c371fc3f9f0e2527adfc5cc9ee1
Aleksander <poczta@zielar.pl>
2025-04-11 06:20:44 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,86 +1,226 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "Point.h"
+#include "SECP256k1.h"
+#include <stdexcept>
+#include <sstream>
 
-Point::Point() {
+const Int Point::P = SECP256k1::P;
+const Int Point::A = SECP256k1::A;
+const Int Point::B = SECP256k1::B;
+const Point Point::G(SECP256k1::Gx, SECP256k1::Gy);
+
+Point::Point() : x(0), y(0), compressed(true) {}
+
+Point::Point(const Int& x, const Int& y, bool compressed) 
+    : x(x), y(y), compressed(compressed) {
+    checkOnCurve();
 }
 
-Point::Point(const Point &p) {
-  x.Set((Int *)&p.x);
-  y.Set((Int *)&p.y);
-  z.Set((Int *)&p.z);
+Point::Point(const std::array<unsigned char, COMPRESSED_SIZE>& data) {
+    if (!parse(data.data(), data.size())) {
+        throw std::runtime_error("Invalid compressed point data");
+    }
 }
 
-Point::Point(Int *cx,Int *cy,Int *cz) {
-  x.Set(cx);
-  y.Set(cy);
-  z.Set(cz);
+Point::Point(const std::array<unsigned char, UNCOMPRESSED_SIZE>& data) {
+    if (!parse(data.data(), data.size())) {
+        throw std::runtime_error("Invalid uncompressed point data");
+    }
 }
 
-Point::Point(Int *cx, Int *cz) {
-  x.Set(cx);
-  z.Set(cz);
+bool Point::parse(const unsigned char* data, size_t size) {
+    if (size == COMPRESSED_SIZE && (data[0] == COMPRESSED_EVEN || data[0] == COMPRESSED_ODD)) {
+        compressed = true;
+        x.set(data + 1, 32);
+        
+        Int x3 = x;
+        x3.modMul(x3, P);
+        x3.modMul(x, P);
+        
+        Int y2 = x3 + A * x + B;
+        y = y2.modSqrt(P);
+        
+        if ((y.isOdd() && data[0] == COMPRESSED_ODD) || (!y.isOdd() && data[0] == COMPRESSED_EVEN)) {
+            return isValid();
+        }
+        y = P - y;
+        return isValid();
+    }
+    else if (size == UNCOMPRESSED_SIZE && data[0] == UNCOMPRESSED) {
+        compressed = false;
+        x.set(data + 1, 32);
+        y.set(data + 33, 32);
+        return isValid();
+    }
+    return false;
 }
 
-void Point::Clear() {
-  x.SetInt32(0);
-  y.SetInt32(0);
-  z.SetInt32(0);
+void Point::serialize(std::array<unsigned char, COMPRESSED_SIZE>& out) const {
+    if (isInfinity()) {
+        throw std::runtime_error("Cannot serialize infinity point");
+    }
+    
+    out[0] = y.isOdd() ? COMPRESSED_ODD : COMPRESSED_EVEN;
+    x.get(out.data() + 1, 32);
 }
 
-void Point::Set(Int *cx, Int *cy,Int *cz) {
-  x.Set(cx);
-  y.Set(cy);
-  z.Set(cz);
+void Point::serialize(std::array<unsigned char, UNCOMPRESSED_SIZE>& out) const {
+    if (isInfinity()) {
+        throw std::runtime_error("Cannot serialize infinity point");
+    }
+    
+    out[0] = UNCOMPRESSED;
+    x.get(out.data() + 1, 32);
+    y.get(out.data() + 33, 32);
 }
 
-Point::~Point() {
+std::vector<unsigned char> Point::serialize(bool forceUncompressed) const {
+    if (forceUncompressed || !compressed) {
+        std::array<unsigned char, UNCOMPRESSED_SIZE> data;
+        serialize(data);
+        return std::vector<unsigned char>(data.begin(), data.end());
+    } else {
+        std::array<unsigned char, COMPRESSED_SIZE> data;
+        serialize(data);
+        return std::vector<unsigned char>(data.begin(), data.end());
+    }
 }
 
-void Point::Set(Point &p) {
-  x.Set(&p.x);
-  y.Set(&p.y);
+bool Point::isInfinity() const {
+    return x.isZero() && y.isZero();
 }
 
-bool Point::isZero() {
-  return x.IsZero() && y.IsZero();
+bool Point::isValid() const {
+    if (isInfinity()) return true;
+    
+    Int y2 = y;
+    y2.modMul(y2, P);
+    
+    Int x3 = x;
+    x3.modMul(x3, P);
+    x3.modMul(x, P);
+    
+    Int rhs = x3 + A * x + B;
+    rhs.mod(P);
+    
+    return y2.equals(rhs);
 }
 
-void Point::Reduce() {
-
-  Int i(&z);
-  i.ModInv();
-  x.ModMul(&x,&i);
-  y.ModMul(&y,&i);
-  z.SetInt32(1);
-
+bool Point::operator==(const Point& other) const {
+    if (isInfinity() && other.isInfinity()) return true;
+    return x == other.x && y == other.y;
 }
 
-bool Point::equals(Point &p) {
-  return x.IsEqual(&p.x) && y.IsEqual(&p.y) && z.IsEqual(&p.z);
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);
 }
 
-std::string Point::toString() {
+Point Point::operator+(const Point& other) const {
+    Point result(*this);
+    result += other;
+    return result;
+}
 
-  std::string ret;
-  ret  = "X=" + x.GetBase16() + "\n";
-  ret += "Y=" + y.GetBase16() + "\n";
-  ret += "Z=" + z.GetBase16() + "\n";
-  return ret;
+Point& Point::operator+=(const Point& other) {
+    if (isInfinity()) {
+        *this = other;
+    } else if (other.isInfinity()) {
+        // pozostaje bez zmian
+    } else if (x == other.x) {
+        if (y == other.y) {
+            doublePoint();
+        } else {
+            *this = infinity();
+        }
+    } else {
+        addPoint(other);
+    }
+    return *this;
+}
 
+Point Point::operator*(const Int& scalar) const {
+    Point result(*this);
+    result *= scalar;
+    return result;
+}
+
+Point& Point::operator*=(const Int& scalar) {
+    multiply(scalar);
+    return *this;
+}
+
+Point Point::infinity() {
+    return Point();
+}
+
+Point Point::generator() {
+    return G;
+}
+
+void Point::doublePoint() {
+    if (isInfinity()) return;
+    
+    Int slope = x;
+    slope.modMul(slope, P);
+    slope *= 3;
+    slope += A;
+    slope.modMul(Int(2) * y, P);
+    
+    Int x3 = slope;
+    x3.modMul(x3, P);
+    x3 -= Int(2) * x;
+    x3.mod(P);
+    
+    Int y3 = x - x3;
+    y3.modMul(slope, P);
+    y3 -= y;
+    y3.mod(P);
+    
+    x = x3;
+    y = y3;
+}
+
+void Point::addPoint(const Point& other) {
+    if (other.isInfinity()) return;
+    if (isInfinity()) {
+        *this = other;
+        return;
+    }
+    
+    Int slope = y - other.y;
+    slope.modMul(x - other.x, P);
+    
+    Int x3 = slope;
+    x3.modMul(x3, P);
+    x3 -= x + other.x;
+    x3.mod(P);
+    
+    Int y3 = x - x3;
+    y3.modMul(slope, P);
+    y3 -= y;
+    y3.mod(P);
+    
+    x = x3;
+    y = y3;
+}
+
+void Point::multiply(const Int& scalar) {
+    Point result = infinity();
+    Point addend = *this;
+    
+    Int k = scalar;
+    while (!k.isZero()) {
+        if (k.isOdd()) {
+            result += addend;
+        }
+        addend.doublePoint();
+        k >>= 1;
+    }
+    
+    *this = result;
+}
+
+void Point::checkOnCurve() const {
+    if (!isInfinity() && !isValid()) {
+        throw std::runtime_error("Point is not on the curve");
+    }
 }
