d812b85958309df020750d3b557e0ed5e4c15c90
Aleksander <poczta@zielar.pl>
2025-04-27 20:04:52 +0200

    Update GPUEngine.h

--- GPU/GPUEngine.h
+++ GPU/GPUEngine.h
@@ -1,107 +1,104 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef GPUENGINEH
-#define GPUENGINEH
+#pragma once
 
 #include <vector>
+#include <string>
+#include <cstdint>
 #include "../SECP256k1.h"
 
-#define SEARCH_COMPRESSED 0
-#define SEARCH_UNCOMPRESSED 1
-#define SEARCH_BOTH 2
+namespace GPUEngine {
 
-static const char *searchModes[] = {"Compressed","Uncompressed","Compressed or Uncompressed"};
+    enum class SearchMode {
+        Compressed,
+        Uncompressed,
+        Both
+    };
 
-// Number of key per thread (must be a multiple of GRP_SIZE) per kernel call
-#define STEP_SIZE 1024
+    enum class SearchType {
+        P2PKH,
+        P2SH,
+        BECH32
+    };
 
-// Number of thread per block
-#define ITEM_SIZE 28
-#define ITEM_SIZE32 (ITEM_SIZE/4)
-#define _64K 65536
+    static constexpr const char* searchModeStrings[] = {
+        "Compressed",
+        "Uncompressed",
+        "Compressed or Uncompressed"
+    };
 
-typedef uint16_t prefix_t;
-typedef uint32_t prefixl_t;
+    static constexpr size_t STEP_SIZE = 1024;
+    static constexpr size_t ITEM_SIZE = 28;
+    static constexpr size_t ITEM_SIZE32 = ITEM_SIZE / 4;
+    static constexpr size_t _64K = 65536;
 
-typedef struct {
-  uint32_t thId;
-  int16_t  incr;
-  int16_t  endo;
-  uint8_t  *hash;
-  bool mode;
-} ITEM;
+    using PrefixT = uint16_t;
+    using PrefixLT = uint32_t;
 
-// Second level lookup
-typedef struct {
-  prefix_t sPrefix;
-  std::vector<prefixl_t> lPrefixes;
-} LPREFIX;
+    struct Item {
+        uint32_t threadId;
+        int16_t increment;
+        int16_t endomorphism;
+        uint8_t* hash;
+        bool mode;
+    };
 
-class GPUEngine {
+    struct LPrefix {
+        PrefixT shortPrefix;
+        std::vector<PrefixLT> longPrefixes;
+    };
 
-public:
+    class Engine {
+    public:
+        Engine(int threadGroups, int threadsPerGroup, int gpuId, uint32_t maxFound, bool rekey);
+        ~Engine();
 
-  GPUEngine(int nbThreadGroup,int nbThreadPerGroup,int gpuId,uint32_t maxFound,bool rekey);
-  ~GPUEngine();
-  void SetPrefix(std::vector<prefix_t> prefixes);
-  void SetPrefix(std::vector<LPREFIX> prefixes,uint32_t totalPrefix);
-  bool SetKeys(Point *p);
-  void SetSearchMode(int searchMode);
-  void SetSearchType(int searchType);
-  void SetPattern(const char *pattern);
-  bool Launch(std::vector<ITEM> &prefixFound,bool spinWait=false);
-  int GetNbThread();
-  int GetGroupSize();
+        // Deleted copy/move operations
+        Engine(const Engine&) = delete;
+        Engine& operator=(const Engine&) = delete;
+        Engine(Engine&&) = delete;
+        Engine& operator=(Engine&&) = delete;
 
-  bool Check(Secp256K1 *secp);
-  std::string deviceName;
+        void SetPrefixes(const std::vector<PrefixT>& prefixes);
+        void SetPrefixes(const std::vector<LPrefix>& prefixes, uint32_t totalPrefix);
+        bool SetKeys(const std::vector<Point>& points);
+        void SetSearchMode(SearchMode mode);
+        void SetSearchType(SearchType type);
+        void SetPattern(const std::string& pattern);
+        bool Launch(std::vector<Item>& foundItems, bool spinWait = false);
 
-  static void PrintCudaInfo();
-  static void GenerateCode(Secp256K1 *secp, int size);
+        int GetThreadCount() const;
+        static int GetGroupSize();
 
-private:
+        bool Check(Secp256K1* secp);
+        std::string GetDeviceName() const;
 
-  bool callKernel();
-  static void ComputeIndex(std::vector<int> &s, int depth, int n);
-  static void Browse(FILE *f,int depth, int max, int s);
-  bool CheckHash(uint8_t *h, std::vector<ITEM>& found, int tid, int incr, int endo, int *ok);
+        static void PrintCudaInfo();
+        static void GenerateCode(Secp256K1* secp, int size);
 
-  int nbThread;
-  int nbThreadPerGroup;
-  prefix_t *inputPrefix;
-  prefix_t *inputPrefixPinned;
-  uint32_t *inputPrefixLookUp;
-  uint32_t *inputPrefixLookUpPinned;
-  uint64_t *inputKey;
-  uint64_t *inputKeyPinned;
-  uint32_t *outputPrefix;
-  uint32_t *outputPrefixPinned;
-  bool initialised;
-  uint32_t searchMode;
-  uint32_t searchType;
-  bool littleEndian;
-  bool lostWarning;
-  bool rekey;
-  uint32_t maxFound;
-  uint32_t outputSize;
-  std::string pattern;
-  bool hasPattern;
+    private:
+        bool CallKernel();
+        bool CheckHash(const uint8_t* hash, std::vector<Item>& foundItems, 
+                      int threadId, int increment, int endomorphism, int* okCount);
 
-};
+        int totalThreads_;
+        int threadsPerGroup_;
+        PrefixT* devicePrefixes_;
+        PrefixT* hostPrefixes_;
+        uint32_t* devicePrefixLookup_;
+        uint32_t* hostPrefixLookup_;
+        uint64_t* deviceKeys_;
+        uint64_t* hostKeys_;
+        uint32_t* deviceOutput_;
+        uint32_t* hostOutput_;
+        bool initialized_;
+        SearchMode searchMode_;
+        SearchType searchType_;
+        bool isLittleEndian_;
+        bool lostWarning_;
+        bool rekeyEnabled_;
+        uint32_t maxFound_;
+        uint32_t outputSize_;
+        std::string searchPattern_;
+        bool hasPattern_;
+    };
 
-#endif // GPUENGINEH
+} // namespace GPUEngine
