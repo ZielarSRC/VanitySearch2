8fca35888f2f5c429aac50a6615a7980ce041bfa
Aleksander <poczta@zielar.pl>
2025-04-29 00:49:45 +0200

    Update Wildcard.cpp

--- Wildcard.cpp
+++ Wildcard.cpp
@@ -1,64 +1,39 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "Wildcard.h"
+#include <cctype>
+#include <algorithm>
 
-using namespace std;
+bool Wildcard::match(std::string_view str, std::string_view pattern, bool caseSensitive) noexcept {
+    const char* s = str.data();
+    const char* p = pattern.data();
+    const char* strPtr = s;
+    const char* patternPtr = p;
+    const char* starPos = nullptr;
+    const char* strAfterStar = nullptr;
 
+    while (*strPtr) {
+        if (*patternPtr == '?' || 
+            (caseSensitive ? *strPtr == *patternPtr 
+                          : std::tolower(*strPtr) == std::tolower(*patternPtr))) {
+            strPtr++;
+            patternPtr++;
+        } 
+        else if (*patternPtr == '*') {
+            starPos = patternPtr;
+            strAfterStar = strPtr;
+            patternPtr++;
+        } 
+        else if (starPos) {
+            patternPtr = starPos + 1;
+            strPtr = ++strAfterStar;
+        } 
+        else {
+            return false;
+        }
+    }
 
-bool Wildcard::match(const char *str, const char *pattern, bool caseSensitive) {
+    while (*patternPtr == '*') {
+        patternPtr++;
+    }
 
-  const char *s;
-  const char *p;
-  bool star = false;
-
-loopStart:
-  for (s = str, p = pattern; *s; ++s, ++p) {
-
-    switch (*p) {
-    case '?':
-      if (*s == '.') goto starCheck;
-      break;
-
-    case '*':
-      star = true;
-      str = s, pattern = p;
-      if (!*++pattern) return true;
-      goto loopStart;
-
-    default:
-      if (caseSensitive) {
-        if (*s != *p)
-          goto starCheck;
-      } else {
-        if (tolower(*s) != tolower(*p))
-          goto starCheck;
-      }
-      break;
-    } /* endswitch */
-
-  } /* endfor */
-
-  if (*p == '*') ++p;
-  return (!*p);
-
-starCheck:
-  if (!star) return false;
-  str++;
-  goto loopStart;
-
+    return !*patternPtr;
 }
