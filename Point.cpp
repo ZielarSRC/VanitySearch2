60ff69ebb9436b7443de4f31ee1bdc5e0688d8f8
Aleksander <poczta@zielar.pl>
2025-04-29 01:07:25 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,86 +1,76 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "Point.h"
+#include "Int.h"
+#include <sstream>
 
-Point::Point() {
-}
-
-Point::Point(const Point &p) {
-  x.Set((Int *)&p.x);
-  y.Set((Int *)&p.y);
-  z.Set((Int *)&p.z);
-}
-
-Point::Point(Int *cx,Int *cy,Int *cz) {
-  x.Set(cx);
-  y.Set(cy);
-  z.Set(cz);
-}
-
-Point::Point(Int *cx, Int *cz) {
-  x.Set(cx);
-  z.Set(cz);
-}
+Point::Point(const Int& x, const Int& y, const Int& z) 
+    : x(x), y(y), z(z) {}
 
 void Point::Clear() {
-  x.SetInt32(0);
-  y.SetInt32(0);
-  z.SetInt32(0);
+    x.SetInt32(0);
+    y.SetInt32(0);
+    z.SetInt32(0);
 }
 
-void Point::Set(Int *cx, Int *cy,Int *cz) {
-  x.Set(cx);
-  y.Set(cy);
-  z.Set(cz);
-}
-
-Point::~Point() {
-}
-
-void Point::Set(Point &p) {
-  x.Set(&p.x);
-  y.Set(&p.y);
-}
-
-bool Point::isZero() {
-  return x.IsZero() && y.IsZero();
+bool Point::isZero() const {
+    return x.IsZero() && y.IsZero() && z.IsZero();
 }
 
 void Point::Reduce() {
-
-  Int i(&z);
-  i.ModInv();
-  x.ModMul(&x,&i);
-  y.ModMul(&y,&i);
-  z.SetInt32(1);
-
+    if (z.IsZero()) return;
+    
+    Int zInv = z;
+    zInv.ModInv();
+    
+    Int zInv2;
+    zInv2.ModSquare(zInv);
+    
+    x.ModMul(x, zInv2);
+    y.ModMul(y, zInv2);
+    z.SetInt32(1);
 }
 
-bool Point::equals(Point &p) {
-  return x.IsEqual(&p.x) && y.IsEqual(&p.y) && z.IsEqual(&p.z);
+bool Point::equals(const Point& p) const {
+    Point p1 = *this;
+    Point p2 = p;
+    p1.Reduce();
+    p2.Reduce();
+    return p1.x.IsEqual(p2.x) && p1.y.IsEqual(p2.y);
 }
 
-std::string Point::toString() {
+std::string Point::toString() const {
+    std::ostringstream oss;
+    oss << "(" << x.GetBase16() << ", " << y.GetBase16() << ", " << z.GetBase16() << ")";
+    return oss.str();
+}
 
-  std::string ret;
-  ret  = "X=" + x.GetBase16() + "\n";
-  ret += "Y=" + y.GetBase16() + "\n";
-  ret += "Z=" + z.GetBase16() + "\n";
-  return ret;
+Point Point::Neg() const {
+    Point r;
+    r.x = x;
+    r.y = y;
+    r.y.ModNeg();
+    r.z = z;
+    return r;
+}
 
+void Point::Set(const Point& p) {
+    x.Set(&p.x);
+    y.Set(&p.y);
+    z.Set(&p.z);
+}
+
+bool Point::IsValid() const {
+    if (z.IsZero()) return false;
+    
+    Point reduced = *this;
+    reduced.Reduce();
+    
+    Int y2;
+    y2.ModSquare(reduced.y);
+    
+    Int x3;
+    x3.ModSquare(reduced.x);
+    x3.ModMul(x3, reduced.x);
+    x3.ModAdd(7);
+    
+    return y2.IsEqual(x3);
 }
