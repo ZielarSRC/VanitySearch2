2fff561a04742656ead7b43af073e999453617b0
Aleksander <poczta@zielar.pl>
2025-04-29 00:37:06 +0200

    Update SECP256k1.h

--- SECP256k1.h
+++ SECP256k1.h
@@ -1,77 +1,62 @@
-/*
- * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef SECP256K1H
-#define SECP256K1H
+#ifndef SECP256K1_H
+#define SECP256K1_H
 
 #include "Point.h"
 #include <string>
 #include <vector>
+#include <array>
+#include <span>
 
-// Address type
-#define P2PKH  0
-#define P2SH   1
-#define BECH32 2
-
-class Secp256K1 {
-
-public:
-
-  Secp256K1();
-  ~Secp256K1();
-  void Init();
-  Point ComputePublicKey(Int *privKey);
-  Point NextKey(Point &key);
-  void Check();
-  bool  EC(Point &p);
-
-  void GetHash160(int type,bool compressed,
-    Point &k0, Point &k1, Point &k2, Point &k3,
-    uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
-
-  void GetHash160(int type,bool compressed, Point &pubKey, unsigned char *hash);
-
-  std::string GetAddress(int type, bool compressed, Point &pubKey);
-  std::string GetAddress(int type, bool compressed, unsigned char *hash160);
-  std::vector<std::string> GetAddress(int type, bool compressed, unsigned char *h1, unsigned char *h2, unsigned char *h3, unsigned char *h4);
-  std::string GetPrivAddress(bool compressed, Int &privKey );
-  std::string GetPublicKeyHex(bool compressed, Point &p);
-  Point ParsePublicKeyHex(std::string str, bool &isCompressed);
-
-  bool CheckPudAddress(std::string address);
-
-  static Int DecodePrivateKey(char *key,bool *compressed);
-
-  Point Add(Point &p1, Point &p2);
-  Point Add2(Point &p1, Point &p2);
-  Point AddDirect(Point &p1, Point &p2);
-  Point Double(Point &p);
-  Point DoubleDirect(Point &p);
-
-  Point G;                 // Generator
-  Int   order;             // Curve order
-
-private:
-
-  uint8_t GetByte(std::string &str,int idx);
-
-  Int GetY(Int x, bool isEven);
-  Point GTable[256*32];       // Generator table
-
+enum AddressType {
+    P2PKH = 0,
+    P2SH = 1,
+    BECH32 = 2
 };
 
-#endif // SECP256K1H
+class Secp256K1 {
+public:
+    Secp256K1();
+    ~Secp256K1() = default;
+
+    void Init();
+    Point ComputePublicKey(const Int* privKey);
+    Point NextKey(const Point& key);
+    void Check();
+    bool EC(const Point& p) const;
+
+    void GetHash160(AddressType type, bool compressed,
+                   const Point& k0, const Point& k1, const Point& k2, const Point& k3,
+                   uint8_t* h0, uint8_t* h1, uint8_t* h2, uint8_t* h3) const;
+
+    void GetHash160(AddressType type, bool compressed, const Point& pubKey, uint8_t* hash) const;
+
+    std::string GetAddress(AddressType type, bool compressed, const Point& pubKey) const;
+    std::string GetAddress(AddressType type, bool compressed, const uint8_t* hash160) const;
+    std::vector<std::string> GetAddress(AddressType type, bool compressed, 
+                                      const uint8_t* h1, const uint8_t* h2, 
+                                      const uint8_t* h3, const uint8_t* h4) const;
+    std::string GetPrivAddress(bool compressed, const Int& privKey) const;
+    std::string GetPublicKeyHex(bool compressed, const Point& p) const;
+    Point ParsePublicKeyHex(const std::string& str, bool& isCompressed) const;
+
+    bool CheckPudAddress(const std::string& address) const;
+
+    static Int DecodePrivateKey(const char* key, bool* compressed);
+
+    Point Add(const Point& p1, const Point& p2) const;
+    Point Add2(const Point& p1, const Point& p2) const;
+    Point AddDirect(const Point& p1, const Point& p2) const;
+    Point Double(const Point& p) const;
+    Point DoubleDirect(const Point& p) const;
+
+    Point G;                 // Generator
+    Int order;               // Curve order
+
+private:
+    uint8_t GetByte(const std::string& str, size_t idx) const;
+    Int GetY(const Int& x, bool isEven) const;
+
+    std::array<Point, 256*32> GTable;  // Generator table
+};
+
+#endif // SECP256K1_H
