9e3352dc24e8269135ef33792a4fb2f232b9d515
Aleksander <poczta@zielar.pl>
2025-12-18 06:28:59 +0100

    Update sha512.h

--- hash/sha512.h
+++ hash/sha512.h
@@ -13,14 +13,17 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #ifndef SHA512_H
 #define SHA512_H
+#include <cstddef>
+#include <cstdint>
 #include <string>
 
 void sha512(unsigned char *input, int length, unsigned char *digest);
-void pbkdf2_hmac_sha512(uint8_t *out, size_t outlen,const uint8_t *passwd, size_t passlen,const uint8_t *salt, size_t saltlen,uint64_t iter);
+void pbkdf2_hmac_sha512(uint8_t *out, size_t outlen, const uint8_t *passwd, size_t passlen, const uint8_t *salt,
+                        size_t saltlen, uint64_t iter);
 void hmac_sha512(unsigned char *key, int key_length, unsigned char *message, int message_length, unsigned char *digest);
 
 std::string sha512_hex(unsigned char *digest);
