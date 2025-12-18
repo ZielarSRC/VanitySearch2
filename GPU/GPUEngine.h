72bfc64a11842118ed712287e6e1fff29765a985
Aleksander <poczta@zielar.pl>
2025-12-18 06:13:31 +0100

    Update GPUEngine.h

--- GPU/GPUEngine.h
+++ GPU/GPUEngine.h
@@ -13,26 +13,28 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #ifndef GPUENGINEH
 #define GPUENGINEH
 
+#include <utility>
 #include <vector>
+
 #include "../SECP256k1.h"
 
 #define SEARCH_COMPRESSED 0
 #define SEARCH_UNCOMPRESSED 1
 #define SEARCH_BOTH 2
 
-static const char *searchModes[] = {"Compressed","Uncompressed","Compressed or Uncompressed"};
+static const char *searchModes[] = {"Compressed", "Uncompressed", "Compressed or Uncompressed"};
 
 // Number of key per thread (must be a multiple of GRP_SIZE) per kernel call
 #define STEP_SIZE 1024
 
 // Number of thread per block
 #define ITEM_SIZE 28
-#define ITEM_SIZE32 (ITEM_SIZE/4)
+#define ITEM_SIZE32 (ITEM_SIZE / 4)
 #define _64K 65536
 
 typedef uint16_t prefix_t;
@@ -40,9 +42,9 @@ typedef uint32_t prefixl_t;
 
 typedef struct {
   uint32_t thId;
-  int16_t  incr;
-  int16_t  endo;
-  uint8_t  *hash;
+  int16_t incr;
+  int16_t endo;
+  uint8_t *hash;
   bool mode;
 } ITEM;
 
@@ -53,18 +55,16 @@ typedef struct {
 } LPREFIX;
 
 class GPUEngine {
-
-public:
-
-  GPUEngine(int nbThreadGroup,int nbThreadPerGroup,int gpuId,uint32_t maxFound,bool rekey);
+ public:
+  GPUEngine(int nbThreadGroup, int nbThreadPerGroup, int gpuId, uint32_t maxFound, bool rekey);
   ~GPUEngine();
   void SetPrefix(std::vector<prefix_t> prefixes);
-  void SetPrefix(std::vector<LPREFIX> prefixes,uint32_t totalPrefix);
+  void SetPrefix(std::vector<LPREFIX> prefixes, uint32_t totalPrefix);
   bool SetKeys(Point *p);
   void SetSearchMode(int searchMode);
   void SetSearchType(int searchType);
   void SetPattern(const char *pattern);
-  bool Launch(std::vector<ITEM> &prefixFound,bool spinWait=false);
+  bool Launch(std::vector<ITEM> &prefixFound, bool spinWait = false);
   int GetNbThread();
   int GetGroupSize();
 
@@ -73,13 +73,14 @@ public:
 
   static void PrintCudaInfo();
   static void GenerateCode(Secp256K1 *secp, int size);
+  static std::vector<int> GetAutoGpuIds(int limit = 8);
+  static std::pair<int, int> GetAutoGridSize(int gpuId);
 
-private:
-
+ private:
   bool callKernel();
   static void ComputeIndex(std::vector<int> &s, int depth, int n);
-  static void Browse(FILE *f,int depth, int max, int s);
-  bool CheckHash(uint8_t *h, std::vector<ITEM>& found, int tid, int incr, int endo, int *ok);
+  static void Browse(FILE *f, int depth, int max, int s);
+  bool CheckHash(uint8_t *h, std::vector<ITEM> &found, int tid, int incr, int endo, int *ok);
 
   int nbThread;
   int nbThreadPerGroup;
@@ -101,7 +102,6 @@ private:
   uint32_t outputSize;
   std::string pattern;
   bool hasPattern;
-
 };
 
-#endif // GPUENGINEH
+#endif  // GPUENGINEH
