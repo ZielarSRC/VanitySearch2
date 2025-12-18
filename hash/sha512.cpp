fb89c2427f1aa65da99093f33e200da15010bd4f
Aleksander <poczta@zielar.pl>
2025-12-18 06:50:22 +0100

    Update sha512.cpp

--- hash/sha512.cpp
+++ hash/sha512.cpp
@@ -17,6 +17,7 @@
 
 #include <string.h>
 
+#include <cstddef>
 #include <cstdint>
 
 #include "sha512.h"
@@ -57,17 +58,181 @@ inline void T2(const uint64_t &a, const uint64_t &b, const uint64_t &c, uint64_t
   temp = (ROR64(a, 28) ^ ROR64(a, 34) ^ ROR64(a, 39)) + ((a & b) ^ (a & c) ^ (b & c));
 }
 
-void transform(const unsigned char *message, uint64_t *w, uint64_t *digest) {}
-void sha512(const unsigned char *message, int len, unsigned char *digest) {}
-void hmac_sha512(const unsigned char *key, int keylen, const unsigned char *msg, int msglen, unsigned char *digest) {}
+void transform(const unsigned char *message, uint64_t *w, uint64_t *digest) {
+  uint64_t a = digest[0];
+  uint64_t b = digest[1];
+  uint64_t c = digest[2];
+  uint64_t d = digest[3];
+  uint64_t e = digest[4];
+  uint64_t f = digest[5];
+  uint64_t g = digest[6];
+  uint64_t h = digest[7];
+  uint64_t t1, t2, tmp;
+
+  memcpy(w, message, SHA512_BLOCK_SIZE);
+
+#ifdef BSWAP
+  for (int i = 0; i < 16; i++) w[i] = SWAP64(w[i]);
+#endif
+
+  for (int i = 16; i < 80; i++) w[i] = SIGMA_1(w[i - 2]) + w[i - 7] + SIGMA_0(w[i - 15]) + w[i - 16];
+
+  for (int i = 0; i < 80; ++i) {
+#ifdef UNROLLED
+#define UNROLLED64
+#endif
+#ifdef UNROLLED64
+    t1 = h + SIGMA1(e) + Ch(e, f, g) + K[i] + w[i];
+    t2 = SIGMA0(a) + Maj(a, b, c);
+    h = g;
+    g = f;
+    f = e;
+    e = d + t1;
+    d = c;
+    c = b;
+    b = a;
+    a = t1 + t2;
+#else
+    T1(e, f, g, h, K[i], w[i], t1);
+    T2(a, b, c, t2);
+    h = g;
+    g = f;
+    f = e;
+    e = d + t1;
+    d = c;
+    c = b;
+    b = a;
+    a = t1 + t2;
+#endif
+  }
+
+  digest[0] += a;
+  digest[1] += b;
+  digest[2] += c;
+  digest[3] += d;
+  digest[4] += e;
+  digest[5] += f;
+  digest[6] += g;
+  digest[7] += h;
+}
+
+void sha512(const unsigned char *message, int len, unsigned char *digest) {
+  static const uint64_t SHA512_hInit[] = {0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL, 0x3c6ef372fe94f82bULL,
+                                          0xa54ff53a5f1d36f1ULL, 0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
+                                          0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL};
+
+  uint8_t msg[SHA512_BLOCK_SIZE];
+  uint64_t digestTmp[8];
+
+  for (int i = 0; i < 8; i++) digestTmp[i] = SHA512_hInit[i];
+
+  while (len >= SHA512_BLOCK_SIZE) {
+    transform(message, (uint64_t *)msg, digestTmp);
+    message += SHA512_BLOCK_SIZE;
+    len -= SHA512_BLOCK_SIZE;
+  }
+
+  memcpy(msg, message, len);
+  msg[len++] = 0x80;
+  if (len > 112) {
+    memset(msg + len, 0x00, SHA512_BLOCK_SIZE - len);
+    transform(msg, (uint64_t *)msg, digestTmp);
+    len = 0;
+  }
+
+  memset(msg + len, 0x00, 120 - len);
+  msg[127] = 8 * len;
+  transform(msg, (uint64_t *)msg, digestTmp);
+
+#ifdef BSWAP
+  for (int i = 0; i < 8; i++) digestTmp[i] = SWAP64(digestTmp[i]);
+#endif
+
+  memcpy(digest, digestTmp, SHA512_HASH_LENGTH);
+}
+
+void hmac_sha512(const unsigned char *key, int keylen, const unsigned char *msg, int msglen, unsigned char *digest) {
+  unsigned char kopad[128 + 64];
+  unsigned char kipad[128 + 64];
+  unsigned char ihash[64];
+
+  if (keylen > 128) {
+    sha512((unsigned char *)key, keylen, ihash);
+    keylen = 64;
+    key = ihash;
+  }
+
+  memset(kopad, 0x5c, 128);
+  memset(kipad, 0x36, 128);
+
+  for (int i = 0; i < keylen; i++) {
+    kopad[i] ^= key[i];
+    kipad[i] ^= key[i];
+  }
+
+  memcpy(kipad + 128, msg, msglen);
+
+  sha512(kipad, 128 + msglen, ihash);
+  memcpy(kopad + 128, ihash, 64);
+  sha512(kopad, 192, digest);
+}
+
 void pbkdf2_hmac_sha512(unsigned char *out, size_t outlen, const unsigned char *p, size_t plen, const unsigned char *s,
-                        size_t slen, uint64_t iter) {}
+                        size_t slen, uint64_t iter) {
+  // Check size
+  if (outlen > 0xFFFFFFFF) {
+    return;
+  }
+
+  unsigned char ihash[64];
+  unsigned char ihash2[64];
+  uint8_t iobuf[4];
+  size_t j;
+
+  memset(ihash, 0, sizeof(ihash));
+  memset(ihash2, 0, sizeof(ihash2));
+
+  hmac_sha512(p, (int)plen, s, (int)slen, ihash);
+
+  for (uint32_t i = 0, blocks = (uint32_t)ceil((double)outlen / (double)64); i < blocks; i++) {
+    iobuf[0] = (i + 1 >> 24) & 0xff;
+    iobuf[1] = (i + 1 >> 16) & 0xff;
+    iobuf[2] = (i + 1 >> 8) & 0xff;
+    iobuf[3] = (i + 1 >> 0) & 0xff;
+
+    memcpy(ihash2, s, slen);
+    memcpy(ihash2 + slen, iobuf, 4);
+    hmac_sha512(p, (int)plen, ihash2, (int)slen + 4, ihash2);
+
+    for (int j = 0; j < 64; j++) ihash[j] ^= ihash2[j];
+
+    for (uint64_t k = 1; k < iter; k++) {
+      memcpy(ihash2, ihash, 64);
+      hmac_sha512(p, (int)plen, ihash2, 64, ihash2);
+      for (int j = 0; j < 64; j++) ihash[j] ^= ihash2[j];
+    }
+
+    for (j = 0; j < 64 && 64 * i + j < outlen; j++) out[64 * i + j] = ihash[j];
+  }
+}
 
 }  // namespace _sha512
 
-void sha512(unsigned char *input, int length, unsigned char *digest) {}
+void sha512(unsigned char *input, int length, unsigned char *digest) { _sha512::sha512(input, length, digest); }
+
 void hmac_sha512(unsigned char *key, int key_length, unsigned char *message, int message_length,
-                 unsigned char *digest) {}
+                 unsigned char *digest) {
+  _sha512::hmac_sha512(key, key_length, message, message_length, digest);
+}
+
 void pbkdf2_hmac_sha512(uint8_t *out, size_t outlen, const uint8_t *passwd, size_t passlen, const uint8_t *salt,
-                        size_t saltlen, uint64_t iter) {}
-std::string sha512_hex(unsigned char *digest) { return ""; }
+                        size_t saltlen, uint64_t iter) {
+  _sha512::pbkdf2_hmac_sha512(out, outlen, passwd, passlen, salt, saltlen, iter);
+}
+
+std::string sha512_hex(unsigned char *digest) {
+  char buf[2 * 64 + 1];
+  buf[2 * 64] = 0;
+  for (int i = 0; i < 64; i++) sprintf(buf + i * 2, "%02x", digest[i]);
+  return std::string(buf);
+}
